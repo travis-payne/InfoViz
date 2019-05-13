2db630652154b6893ff109932e85002f1282bbc3
Travis Payne <travis.payne@uea.ac.uk>
2019-05-13 18:43:01 +0100

    Big changes, sweet pie chart

--- charts.js
+++ charts.js
@@ -1,12 +1,13 @@
 $(function(){
 
    male_female_rates();
+   drilldown();
 
 
 });
 
 
-function male_female_rates(){Highcharts.chart('container', {
+function male_female_rates(){Highcharts.chart('Initial-bar', {
 
     title: {
         text: 'A comparison of Male and Female suicide rates'
@@ -74,29 +75,6 @@ function male_female_rates(){Highcharts.chart('container', {
             , 18.83596078,18.27333333, 18.02568182,17.68637597
             , 18.3218107,17.84970833,17.56412393,17.53467742,22.02375
         ]
-    },
-    {
-        type: 'pie',
-        name: 'Total consumption',
-        data: [{
-            name: 'Jane',
-            y: 13,
-            color: Highcharts.getOptions().colors[0] // Jane's color
-        }, {
-            name: 'John',
-            y: 23,
-            color: Highcharts.getOptions().colors[1] // John's color
-        }, {
-            name: 'Joe',
-            y: 19,
-            color: Highcharts.getOptions().colors[2] // Joe's color
-        }],
-        center: [100, 0],
-        size: 100,
-        showInLegend: false,
-        dataLabels: {
-            enabled: true
-        }
     }],
 
     responsive: {
@@ -118,4 +96,178 @@ function male_female_rates(){Highcharts.chart('container', {
 
 
 
+function side_by_side(){Highcharts.chart('pie-comparison', {
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+    },
+    title: {
+        text: 'Number of Suicides by Gender and Age'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                style: {
+                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                },
+                connectorColor: 'silver'
+            }
+        }
+    },
+    labels: {
+        items: [{
+            html: '<p style="font-size:18px;font-weight:bold;">Male</p>',
+            style: {
+                left: '125px',
+                top: '18px',
+                color: (Highcharts.theme && Highcharts.theme.textColor) || 'black'
+            }
+        },
+        {
+            html: '<p style="font-size:18px;font-weight:bold;">Female</p>',
+            style: {
+                left: '725px',
+                top: '18px',
+                color: (Highcharts.theme && Highcharts.theme.textColor) || 'black'
+            }
+        }]
+    },
+    series: [{
+        name: 'Male',
+        type: 'pie',
+        data: [
+            { name: '35-54', y: 1945908 },
+            { name: '55-74', y: 1228407 },
+            { name: '25-34', y: 915089 },
+            { name: '15-24', y: 633105 },
+            { name: '75+', y: 431134 },
+            { name: '5-14', y: 35267 },
+        ],
+        center: ['25%','50%'],
+        size:'80%',
 
+    },
+    {
+        name: 'Female',
+        type: 'pie',
+        data: [
+            { name: '35-54', y: 506233 },
+            { name: '55-74', y: 430036 },
+            { name: '25-34', y: 208823 },
+            { name: '15-24', y: 175437 },
+            { name: '75+', y: 221984 },
+            { name: '5-14', y: 16997 }
+        ],
+        center: ['75%','50%'],
+        size:'80%',
+        showInLegend: true
+    }]
+});
+
+}
+
+
+
+
+
+function drilldown(){Highcharts.chart('pie-comparison', {
+
+    chart: {
+        type: 'pie',
+        styleMode:true
+        
+    },
+    title: {
+        text: 'Numbers by Gender and Age',
+        align: 'center',
+    },
+
+    subtitle: {
+        text: 'Source: www.worldometers.info',
+    },
+    plotOptions: {
+        series:{
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}: {point.percentage:.1f} %',
+            }
+
+        },
+        pie: {
+            size: '120%',
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: false
+            },
+            showInLegend: true
+        }
+    },
+    tooltip: {
+        headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+        pointFormat: '{point.name}: <b>{point.percentage:.1f}%</b>'
+    },
+
+    series: [ 
+    {
+
+        name: "Genders",
+        allowPointSelect: true,
+        colorByPoint: true,
+        data:[
+            {   
+                name: 'Male',
+                y: 5188910,
+                drilldown: 'Male'
+            },
+            {   
+                name: 'Female',
+                y: 1559510,
+                drilldown: 'Female'
+            },
+        ]
+    },
+  ],
+
+  drilldown:{
+        series: [
+            {
+                allowPointSelect: true,
+                name: "Male",
+                id: "Male",
+                data: [
+                        ["35-54",1945908],
+                        ["55-74",1228407],
+                        ["25-34",915089],
+                        ["15-24",633105],
+                        ["75+",431134],
+                        ["5-14",35267],  
+                ]
+            },
+            {
+                allowPointSelect: true,
+                name: "Female",
+                id: "Female",
+                data: [
+                        ["35-54",506233],
+                        ["55-74",430036],
+                        ["25-34",208823],
+                        ["15-24",175437],
+                        ["75+",221984],
+                        ["5-14",16997],  
+                ]
+            }
+        ]
+
+  }
+
+
+});}
